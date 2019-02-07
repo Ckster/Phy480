9e930fcdc36c729b59c001c973729da8c00afaeb
Ckster <verleyee@msu.edu>
2019-02-06 23:00:01 -0500

    HW_1_bonus
    Code showing randomness of machine error and error plot, with data file.

---
+++ random_error.cpp
@@ -0,0 +1,27 @@
+#include <iostream>	     // this has the cout, cin definitions
+#include <iomanip>
+using namespace std;     // if omitted, then need std::cout, std::cin
+#include <fstream>
+#include<math.h>
+
+int
+main ()
+{
+  ofstream my_file; // add this to initialize output data file
+  my_file.open("random.dat");
+  float x_f = 0. ;
+  double x_d = 0.;
+  double error;
+
+  for (int i = 1; i < 2000; i+=1){  //create a loop to take the square root of a bunch of numbers
+    x_f=sqrt(float(i));
+    x_d=sqrt(double(i));
+    error = ((x_d - x_f)/(x_f));   // take the relative error but keep the sign, so on abs
+    my_file<<" "<<i<<" " << x_d << " " << x_f << " " << error << " " << endl;
+  }
+  my_file.close();
+  return 0;
+
+
+
+}
